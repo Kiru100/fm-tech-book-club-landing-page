308eb953a2fccd242a589146cfa483e5d083d848
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2025-01-29 21:29:26 +0800

    Update index.tsx

--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -4,10 +4,14 @@ import Logo from '@/app/assets/images/logo.svg';
 
 export default function Hero() {
     return (
-        <div className="pt-6 px-4">
+        <div className="pt-6 px-4 bg-tbc-neutral-100">
             <div className=''>
                 <Image src={Logo} alt="hero" />
-               
+               <h1>
+                    Join the 
+                    ultimate tech 
+                    book club
+               </h1>
             </div>
         </div>
     )
