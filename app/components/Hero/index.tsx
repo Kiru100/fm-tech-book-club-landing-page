b059d534755ddc6a44772c796c59c7986e5e4c99
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2025-02-02 00:36:03 +0800

    Update index.tsx

--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -7,11 +7,15 @@ export default function Hero() {
         <div className="pt-6 px-4 bg-tbc-neutral-100">
             <div className=''>
                 <Image src={Logo} alt="hero" />
-               <h1>
-                    Join the 
-                    ultimate tech 
-                    book club
-               </h1>
+                <div className="mt-12">
+                    <h1 className="bg-text-gradient bg-clip-text text-transparent text-4xl font-martian-mono-bold leading-[120%]">
+                        Join the <br />
+                        ultimate tech<br />
+                        book club
+                    </h1>
+                    <p className="mt-6 text-xl leading-[140%] font-inter-normal">Turn your reading time into learning time with fellow tech enthusiasts. Get curated recommendations, join vibrant discussions, and level up your skills one chapter at a time.</p>
+                </div>
+               
             </div>
         </div>
     )
