63f48a5fc4dbe1a1d5f8b32d1ad1bad2bf7628e9
Kiru100 <79371033+Kiru100@users.noreply.github.com>
2025-02-09 20:18:35 +0800

    Update index.tsx

--- app/components/MembershipOptionSection/index.tsx
+++ app/components/MembershipOptionSection/index.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function MembershipOptionSection() {
     return (
-        <div>
-        
+        <div className="mt-16 px-4">
+             <h2 className="text-tbc-neutral-900 font-martian-mono-semibold text-[34px] leading-[130%] -tracking-[2px]">
+                Membership <br/>
+                options <br/>
+            </h2>
         </div>
     )
 }
