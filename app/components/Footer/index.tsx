640830f2139aeb2d3fff7770f1caebbb2f9784e6
Noel Dasco Jr. <79371033+Kiru100@users.noreply.github.com>
2025-10-17 23:01:28 +0800

    Improve Footer layout and responsiveness
    Updated class names in the Footer component to enhance grid alignment, center content, and improve responsive behavior for headings and layout on larger screens.

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -9,14 +9,14 @@ function Footer() {
     return (
         <div className="mt-16 bg-tbc-neutral-900 bg-square-background-dark rounded-t-2xl pt-12 px-4 md:p-8">
             <div>
-                <h2 className="text-tbc-neutral-100 font-martian-mono-semibold leading-[130%] -tracking-[2px] text-[2.125rem] text-center">Ready to debug your reading list?</h2>
+                <h2 className="text-tbc-neutral-100 font-martian-mono-semibold leading-[130%] -tracking-[2px] text-[2.125rem] md:text-center">Ready to debug your reading list?</h2>
             </div>
-            <div className="mt-10 md:justify-center">
+            <div className="mt-10 display-grid justify-items-center">
                 <ReviewMemberShipOptions mode="dark" />
             </div>
-            <div className="flex flex-col mt-16 py-6 px-8 items-center gap-y-4 border-t border-tbc-neutral-700">
+            <div className="flex flex-col mt-16 py-6 px-8 items-center gap-y-4 border-t border-tbc-neutral-700 md:justify-between md:flex-row">
                 <p className="text-tbc-neutral-100 font-martian-mono-normal leading-[120%] -tracking-[1px] text-sm">© 2024 – Tech Book Club</p>
-                <div className="flex flex-row gap-x-6 mt-">
+                <div className="flex flex-row gap-x-6 ">
                     <Link href="/"><Image src={logo_bluesky} alt="Blue Sky Logo" height={22} width={22} /></Link>
                     <Link href="/"><Image src={logo_linkedin} alt="Linked In Logo" height={22} width={22} /></Link>
                 </div>
