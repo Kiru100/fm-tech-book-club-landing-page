6ff617eb80632e9742bacde8a32f4c4214ce6376
Noel Dasco Jr. <79371033+Kiru100@users.noreply.github.com>
2025-10-19 00:27:33 +0800

    fix read together section on large screens

--- app/components/ReadTogetherSection/index.tsx
+++ app/components/ReadTogetherSection/index.tsx
@@ -10,31 +10,35 @@ import image_together_bg from "@/app/assets/images/image-read-together-mobile.we
  */
 function ReadTogetherSection() {
     return (
-        <section className="mt-16 px-4 md:p-8">
-            <h2 className="text-tbc-neutral-900 font-martian-mono-semibold text-[34px] leading-[130%] -tracking-[2px] md:text-[3.125rem]">
-                Read together, <br/>
-                grow together
-            </h2>
-            <div className="grid mt-6 gap-4">
-                <div className="flex flex-row gap-[14px]">
-                    <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
-                    <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Monthly curated tech reads selected by industry experts</p>
-                </div>
-                <div className="flex flex-row gap-[14px]">
-                    <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
-                    <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Virtual and in-person meetups for deep-dive discussions</p>
-                </div>
-                <div className="flex flex-row gap-[14px]">
-                    <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
-                    <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Early access to new tech book releases</p>
-                </div>
-                <div className="flex flex-row gap-[14px]">
-                    <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
-                    <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Author Q&A sessions with tech thought leaders</p>
-                </div>
-            </div>  
-            <div className="mt-10 rounded-[12.25px] overflow-hidden">
-                <Image src={image_together_bg} alt="People reading together." />
+        <section className="mt-16 px-4 md:p-8 lg:container lg:mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
+            {/* 1st Column */}
+            <div className="lg:order-2">
+                <h2 className="text-tbc-neutral-900 font-martian-mono-semibold text-[34px] leading-[130%] -tracking-[2px] md:text-[3.125rem]">
+                    Read together, <br/>
+                    grow together
+                </h2>
+                <div className="grid mt-6 gap-4">
+                    <div className="flex flex-row gap-[14px]">
+                        <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
+                        <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Monthly curated tech reads selected by industry experts</p>
+                    </div>
+                    <div className="flex flex-row gap-[14px]">
+                        <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
+                        <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Virtual and in-person meetups for deep-dive discussions</p>
+                    </div>
+                    <div className="flex flex-row gap-[14px]">
+                        <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
+                        <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Early access to new tech book releases</p>
+                    </div>
+                    <div className="flex flex-row gap-[14px]">
+                        <Image src={check_box_icon} alt="Rating Image Avatars" height={24} width={24} />
+                        <p className="text-tbc-neutral-700 font-inter-normal leading-[140%] -tracking-[0.5px] text-xl">Author Q&A sessions with tech thought leaders</p>
+                    </div>
+                </div>  
+            </div>
+            {/* 2nd Column */}
+            <div className="mt-10 rounded-[12.25px] overflow-hidden lg:order-1">
+                <Image src={image_together_bg} alt="People reading together." className="w-full h-full object-cover" />
             </div>
         </section>
     )
